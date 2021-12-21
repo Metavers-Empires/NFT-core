d19ea4d6a27191c72db20822e14546ac19483254
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-12-21 12:35:50 -0600

    Define step #1 (Define NFT drop)

---
+++ contracts/NFTDropTenoch.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract TenochNFT {
+
+    /// @dev Define an NFT drop object with an struct.
+    struct Drop {
+        string imageURI;
+        string name;
+        string description;
+        string twitter;
+        string instagram;
+        string websiteURI;
+        string price;
+        uint256 supply;
+        uint256 presale;
+        uint256 sale;
+        uint8 chain;
+        bool approved;
+    }
+
+    /// @dev Create a list of some sort to hold all the objects
+
+    /// @dev Get the NFT drop objects list
+
+    /// @dev Add to the NFT drop objects list
+
+    /// @dev Remove from the NFT drop objects list
+
+    /// @dev Approve an NFT drop object to enable displaying
+
+    /// @dev Clear out all NFT drop objects from list
+}
