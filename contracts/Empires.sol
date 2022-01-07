8b1720cc95d4a1845cb4944b2a9c2a80f8ef1251
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-01-07 17:57:34 -0600

    Adding more utilities

--- contracts/Empires.sol
+++ contracts/Empires.sol
@@ -1,21 +1,31 @@
 // SPDX-License-Identifier: MIT
+/// @title Smart contract of the Empires Punk
+/// @author Rafael Fuentes Rangel
 pragma solidity ^0.8.2;
-
+/// @dev herences included
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-
+import "@openzeppelin/contracts/utils/Counters.sol";
+/// @dev adding herences from OpenZeppelin
 contract EmpiresPunks is ERC721, ERC721Enumerable {
+    /// @dev using libraries of Counters
+    using Counters for Counters.Counter;
+    Counters.Counters private _idCounter;
+    /// @dev Constructor adding the name and token
     constructor() ERC721("Empires Punks", "EM") {}
-
-    // The following functions are overrides required by Solidity.
-
+    /// @dev The following functions are overrides required by Solidity.
+    /// @dev Function mint added, that will add the tokenIsd with the msg.sender
+    function mint() public {
+        uint256 current = _idCounter.current();
+        _safemint(msg.sender, tokenId); 
+    }
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
         override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId);
     }
-
+    /// @dev anounce!! 🚨
     function supportsInterface(bytes4 interfaceId)
         public
         view
