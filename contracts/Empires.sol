3325d2353b3f68d354162d0563f475d32ccffe59
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-01-08 11:57:29 -0600

    Finishing the smart contract

--- contracts/Empires.sol
+++ contracts/Empires.sol
@@ -11,13 +11,19 @@ contract EmpiresPunks is ERC721, ERC721Enumerable {
     /// @dev using libraries of Counters
     using Counters for Counters.Counter;
     Counters.Counter private _idCounter;
+    /// @dev Maximun of TotalSupply of NFT checked in the constructor
+    uint256 public maxSupply;
     /// @dev Constructor adding the name and token
-    constructor() ERC721("Empires Punks", "EM") {}
+    constructor(uint256 _maxSupply) ERC721("Empires Punks", "EM") {
+        maxSupply = _maxSupply;
+    }
     /// @dev The following functions are overrides required by Solidity.
     /// @dev Function mint added, that will add the tokenIsd with the msg.sender
     function mint() public {
         uint256 current = _idCounter.current();
-        _safeMint(msg.sender, tokenId); 
+        /// @dev checked thar current will be less than MaxSupply
+        require(current < maxSupply);
+        _safeMint(msg.sender, tokenId);
     }
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
